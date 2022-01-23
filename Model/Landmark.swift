e6a4122beecbde916441aa2dc2b862e7e2b23da9
adityagi02 <adityatyagi20bcs126@iiitkottayam.ac.in>
2022-01-23 19:22:48 +0530

    Created the Row View

--- Model/Landmark.swift
+++ Model/Landmark.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
+
 struct Landmark: Hashable, Codable{
     var id: Int
     var name: String
@@ -23,11 +25,10 @@ struct Landmark: Hashable, Codable{
     
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D (
-            latitude: Coordinates.latitude,
-            longitude: Coordinates.longitude)
+           CLLocationCoordinate2D(
+               latitude: coordinates.latitude,
+               longitude: coordinates.longitude)
     }
-    
     struct Coordinates: Hashable, Codable{
         var latitude: Double
         var longitude: Double
